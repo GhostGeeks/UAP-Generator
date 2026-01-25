8949c552399ff0e2f8864c73b6744d7cda1c6ec5
GhostGeeks <txghostgeeks@gmail.com>
2026-01-25 17:30:42 -0600

    Update run.py

--- SpiritBox Module/run.py
+++ SpiritBox Module/run.py
@@ -32,7 +32,12 @@ DEFAULT_SETTINGS = {
 # =====================
 # OLED
 # =====================
-device = ssd1306(i2c(1, 0x3C), width=128, height=64)
+from luma.core.interface.serial import i2c
+from luma.oled.device import ssd1306
+
+serial = i2c(port=1, address=0x3C)
+device = ssd1306(serial, width=128, height=64)
+
 
 # =====================
 # SETTINGS
